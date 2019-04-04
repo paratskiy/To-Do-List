8c5124ea62b225b628a423d5f63dac27a49d3743
Bogdan Paratskiy <bog4242@gmail.com>
2019-04-04 19:34:49 +0300

    created method editElement

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const addTodoList = document.querySelector('#add-todo-list');
 const addTaskBtn = document.querySelector('#add-task');
 
-const parentNode = document.querySelector('.todo-wrap');
+const parentNode = document.querySelector('.todo-list-wrap');
 
 let todoList = new TodoList(parentNode);
 
--- js/todo.js
+++ js/todo.js
@@ -15,24 +15,25 @@ class TodoList extends Global{
         let x = prompt('Введите название листа');
 
         const logoTodoList = this.createElement('i', { class: 'far fa-calendar-alt calendar' });
-        const titleTodoList = this.createElement('input', { type: 'text', name: 'title', id: 'title', value: x });
+        const titleTodoList = this.createElement('label', { class: 'title todo-title'}, x);
+        const editTodoTitle = this.createElement('input', {type: 'text', name: 'edit-todo-title', class: 'edit edit-todo-title hide', value: x});
         const editTodoListBtn = this.createElement('i', { class: 'fas fa-pen edit' });
         const deleteTodoListBtn = this.createElement('i', { class: 'far fa-trash-alt delete' });
         const todoListHeader = this.createElement('form', {
-            id: 'todo-list-header',
+            class: 'todo-list-header',
             onsubmit: 'return false'
-        }, logoTodoList, titleTodoList, editTodoListBtn, deleteTodoListBtn);
+        }, logoTodoList, titleTodoList, editTodoTitle, editTodoListBtn, deleteTodoListBtn);
 
         const logoAddTodoItem = this.createElement('i', { class: 'fas fa-plus' });
         const inputAddTodoItem = this.createElement('input', {
             type: 'text',
             name: 'add-todo-item',
-            id: 'add-todo-item',
+            class: 'add-todo-item',
             placeholder: 'Start typing here to create a task...'
         });
-        const addTodoItemBtn = this.createElement('button', { id: 'add-todo-item', type: 'button' }, 'Add Task');
+        const addTodoItemBtn = this.createElement('button', { class: 'add-todo-item', type: 'button' }, 'Add Task');
         const createTodoItem = this.createElement('form', {
-            id: 'create-todo-item',
+            class: 'create-todo-item',
             onsubmit: 'return false'
         }, logoAddTodoItem, inputAddTodoItem, addTodoItemBtn);
 
@@ -43,13 +44,15 @@ class TodoList extends Global{
         this.parent.appendChild(todoListWrap);
         
         addTodoItemBtn.addEventListener('click', () => {
-
             this.addTodoItem(todoList, inputAddTodoItem.value);
-
         });
 
         deleteTodoListBtn.addEventListener('click', () => {
             this.deleteElement(todoListWrap);
+        });
+
+        editTodoListBtn.addEventListener('click', () => {
+            this.editElement(todoListHeader);
         });
 
     }
@@ -57,25 +60,49 @@ class TodoList extends Global{
     addTodoItem(parent, value) {
 
         const doneItem = this.createElement('input', { type: 'checkbox', name: 'done', class: 'done' });
-        const itemTitle = this.createElement('input', { type: 'text', name: 'item-title', class: 'item-title', value: value });
+        const itemTitle = this.createElement('label', {class: 'title item-title'}, value);
+        const editItemTitle = this.createElement('input', { type: 'text', name: 'edit-item-title', class: 'edit edit-item-title hide', value: value });
         const moveItemBtn = this.createElement('i', { class: 'fas fa-arrows-alt-v move' });
         const editItemBtn = this.createElement('i', { class: 'fas fa-pen edit' });
         const deleteItemBtn = this.createElement('i', { class: 'far fa-trash-alt delete' });
-        const item = this.createElement('li', { class: 'todo-item' }, doneItem, itemTitle, moveItemBtn, editItemBtn, deleteItemBtn);
+        const item = this.createElement('li', { class: 'todo-item' }, doneItem, itemTitle, editItemTitle, moveItemBtn, editItemBtn, deleteItemBtn);
 
         parent.appendChild(item);
 
-        
-
-        console.log(value);
     }
 
     deleteElement(el) {
         this.parent.removeChild(el);
     }
 
-    editTodoListBtn() {
+    editElement(el) {
+        
+        const editInput = el.querySelector('input.edit');
+        const editableLabel = el.querySelector('label.title');
 
+        editInput.classList.toggle('hide');
+        editableLabel.classList.toggle('hide');
+
+        editInput.focus();
+        editInput.selectionStart = editInput.value.length;
+
+        editInput.addEventListener('keydown', (event) => {
+            
+            if(event.keyCode === 13){
+                console.log('keypress');    
+                editInput.classList.toggle('hide');
+                editableLabel.classList.toggle('hide');
+
+                editableLabel.innerHTML = editInput.value;
+
+            }
+
+        }, false);
+
+                
+        console.dir(editInput);
+        console.dir(editableLabel.innerHTML);
+        
     }
 
 }
